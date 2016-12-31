d9f74f1a39244ef235a33da5b5e615dcbed1c0b1
NoaSch <noasch4@gmail.com>
2016-12-31 11:55:11 +0200

    add gender to profile

--- PartnerMatcher/PartnerMatcher/AddProfileWin.xaml.cs
+++ PartnerMatcher/PartnerMatcher/AddProfileWin.xaml.cs
@@ -31,10 +31,13 @@ namespace PartnerMatcher
         bool? quiet;
         bool? animals;
         bool? play;
+        string gender;
 
         public AddProfileWin()
         {
             InitializeComponent();
+            comboBoxGen.Items.Add("male");
+            comboBoxGen.Items.Add("female");
         }
 
         private void finishBtn_Click(object sender, RoutedEventArgs e)
@@ -49,70 +52,77 @@ namespace PartnerMatcher
                 System.Windows.MessageBox.Show("Enter a valid age in natural number", "Error");
             }
             else
+
+                gender = comboBoxGen.SelectedValue.ToString();
+            //get all matching rows
+            name = nameTextBox.Text;
+            smoke = checkBoxSmoke.IsChecked;
+            kosher = checkBoxKosher.IsChecked;
+            quiet = checkBoxquiet.IsChecked;
+            animals = checkBoxAnimals.IsChecked;
+            play = checkBoxPlay.IsChecked;
+            OleDbConnection conn = new OleDbConnection();
+            conn.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb";
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.CommandText = "INSERT into Profiles (mail, Pass, age, gender, smoke, fullName, kosher, quiet, animals, play) values(@mail, @Pass,@age,@gender, @smoke,@name, @kosher, @quiet,@animals,@play)";
+            cmd.Connection = conn;
+
+            conn.Open();
+
+            if (conn.State == ConnectionState.Open)
             {
-                //get all matching rows
-                name = nameTextBox.Text;
-                smoke = checkBoxSmoke.IsChecked;
-                kosher = checkBoxKosher.IsChecked;
-                quiet = checkBoxquiet.IsChecked;
-                animals = checkBoxAnimals.IsChecked;
-                play = checkBoxPlay.IsChecked;
-                OleDbConnection conn = new OleDbConnection();
-                conn.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb";
-                OleDbCommand cmd = new OleDbCommand();
-                cmd.CommandText = "INSERT into Profiles (mail, Pass, age, smoke, fullName, kosher, quiet, animals, play) values(@mail, @Pass,@age, @smoke,@name, @kosher, @quiet,@animals,@play)";
-                cmd.Connection = conn;
+                cmd.Parameters.Add("@mail", OleDbType.VarChar).Value = mail;
+                cmd.Parameters.Add("@Pass", OleDbType.VarChar).Value = pass;
+                cmd.Parameters.Add("@age", OleDbType.Integer).Value = age;
+                cmd.Parameters.Add("@gender", OleDbType.VarChar).Value = gender;
+                cmd.Parameters.Add("@smoke", OleDbType.Boolean).Value = smoke;
+                cmd.Parameters.Add("@name", OleDbType.VarChar).Value = name;
+                cmd.Parameters.Add("@kosher", OleDbType.Boolean).Value = kosher;
+                cmd.Parameters.Add("@quiet", OleDbType.Boolean).Value = quiet;
+                cmd.Parameters.Add("@animals", OleDbType.Boolean).Value = animals;
+                cmd.Parameters.Add("@play", OleDbType.Boolean).Value = play;
 
-                conn.Open();
-
-                if (conn.State == ConnectionState.Open)
+                try
                 {
-                    cmd.Parameters.Add("@mail", OleDbType.VarChar).Value = mail;
-                    cmd.Parameters.Add("@Pass", OleDbType.VarChar).Value = pass;
-                    cmd.Parameters.Add("@age", OleDbType.Integer).Value = age;
-                    cmd.Parameters.Add("@smoke", OleDbType.Boolean).Value = smoke;
-                    cmd.Parameters.Add("@name", OleDbType.VarChar).Value = name;
-                    cmd.Parameters.Add("@kosher", OleDbType.Boolean).Value = kosher;
-                    cmd.Parameters.Add("@quiet", OleDbType.Boolean).Value = quiet;
-                    cmd.Parameters.Add("@animals", OleDbType.Boolean).Value = animals;
-                    cmd.Parameters.Add("@play", OleDbType.Boolean).Value = play;
+                    //send the mail to the user's mail
+                    cmd.ExecuteNonQuery();
+                    MailMessage mailMsg = new MailMessage();
+                    SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
 
-                    try
-                    {
-                        //send the mail to the user's mail
-                        cmd.ExecuteNonQuery();
-                        MailMessage mailMsg = new MailMessage();
-                        SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                    mailMsg.From = new MailAddress("partnersmatcherapp@gmail.com");
+                    mailMsg.To.Add(mail);
+                    mailMsg.Subject = name + ", Welcome to Partners Matcher";
+                    mailMsg.Body = "wish you to find the second part of your Activity";
 
-                        mailMsg.From = new MailAddress("partnersmatcherapp@gmail.com");
-                        mailMsg.To.Add(mail);
-                        mailMsg.Subject = name + ", Welcome to Partners Matcher";
-                        mailMsg.Body = "wish you to find the second part of your Activity";
+                    SmtpServer.Port = 587;
+                    SmtpServer.Credentials = new System.Net.NetworkCredential("partnersmatcherapp@gmail.com", "p12345678");
+                    SmtpServer.EnableSsl = true;
 
-                        SmtpServer.Port = 587;
-                        SmtpServer.Credentials = new System.Net.NetworkCredential("partnersmatcherapp@gmail.com", "p12345678");
-                        SmtpServer.EnableSsl = true;
-
-                        SmtpServer.Send(mailMsg);
-                        MessageBox.Show("User Created");
-                        conn.Close();
-                    }
-                    catch (OleDbException ex)
-                    {
-                        MessageBox.Show(ex.Source);
-                        MessageBox.Show(ex.Message);
-                        conn.Close();
-                    }
-
-
+                    SmtpServer.Send(mailMsg);
+                    MessageBox.Show("User Created");
+                    conn.Close();
                 }
-                else
+                catch (OleDbException ex)
                 {
-                    MessageBox.Show("DB Error call support");
+                    MessageBox.Show(ex.Source);
+                    MessageBox.Show(ex.Message);
+                    conn.Close();
                 }
-                Close();
+
+
             }
+            else
+            {
+                MessageBox.Show("DB Error call support");
+            }
+            Close();
+        }
+
+        private void comboBoxGen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            gender = comboBoxGen.SelectedValue.ToString();
         }
     }
+
 }
 
