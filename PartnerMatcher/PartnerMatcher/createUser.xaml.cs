0e5ac92a2ce804e34efa56ca47a671e62852cf52
NoaSch <noasch4@gmail.com>
2017-01-07 10:22:18 +0200

    version 3  change gender selection

--- PartnerMatcher/PartnerMatcher/createUser.xaml.cs
+++ PartnerMatcher/PartnerMatcher/createUser.xaml.cs
@@ -39,7 +39,7 @@ namespace PartnerMatcher
 
             else if (checkExistMail(mailTxt.Text))
             {
-                System.Windows.MessageBox.Show("The E-mail already in the system", "Error");
+                System.Windows.MessageBox.Show("This E-mail is already in the system", "Error");
 
             }
             else
